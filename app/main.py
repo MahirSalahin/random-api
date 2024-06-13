3235ebdb0c1d68067d524bdfd3001e38bf219c66
MahirSalahin <mahirsalahin01@gmail.com>
2024-06-14 02:42:27 +0600

    Rearrange File Structure

--- app/main.py
+++ app/main.py
@@ -1,24 +1,16 @@
-from fastapi import FastAPI
-from routers.random import random_router
+from fastapi import FastAPI, Request, routing
+from routers.random import router as randoom_router
 
 app = FastAPI()
-app.include_router(random_router)
+app.include_router(randoom_router)
 
 @app.get("/")
-def get_home() -> dict:
+def get_home(request: Request) -> dict:
     """Return the API home page with available routes."""
-    routes = {
-        "/": "Description of the api",
-        "/random": "Get a random float between 0 and 1.",
-        "/random/int": "Get a random integer",
-        "/random/range/{left}/{right}": "Get a random integer within the specified range.",
-        "/random/char": "Get a random ASCII character.",
-        "/random/coinflip": "Simulate a coin toss and get the result.",
-        "/random/choice/{options}": "Select a random option from the given list.",
-        "/random/color": "Select a random hex color.",
-        "/random/user": "Generate a random user with name, email, and password.",
-        "/random/password/{length}": "Generate a random password of a specified length.",
-    }
+    routes = {}
+    for route in request.app.routes:
+        if isinstance(route, routing.APIRoute):
+            routes[route.path] = route.name
     meta = {
         "api_version": "1.0",
         "docs": "https://github.com/MahirSalahin/random_api"
--- app/routers/random.py
+++ app/routers/random.py
@@ -1,90 +1,68 @@
 from fastapi import APIRouter, HTTPException
-import random
 import json
-import secrets
 import string
-import faker
 from utils.utils import *
 
 
-random_router = APIRouter()
+router = APIRouter(prefix="/random")
 
-@random_router.get("/random")
+
+@router.get("/")
 def get_root() -> dict:
     """Return a random float between 0 and 1."""
-    return {"float": random.random()}
+    return {"float": random_float()}
 
 
-@random_router.get("/random/int")
+@router.get("/int")
 def get_random_int() -> dict:
     """Return a random integer."""
-    return {"int": generate_random_int()}
+    return {"int": random_int()}
 
 
-@random_router.get("/random/range/{left}/{right}")
+@router.get("/{left}/{right}")
 def get_random_in_range(left: int, right: int) -> dict:
     """Return a random integer within the given range."""
     if left > right:
         raise HTTPException(status_code=400, detail="Invalid range")
-    return {"int": generate_random_int(left, right)}
+    return {"int": random_int(left, right)}
 
 
-@random_router.get("/random/char")
+@router.get("/char")
 def generate_random_char() -> dict:
     """Get a random ASCII character."""
-    return {"char": random.choice(string.ascii_letters)}
+    return {"char": random_choice(string.ascii_letters)}
 
 
-@random_router.get("/random/coinflip")
+@router.get("/coinflip")
 def toss_coin() -> dict:
     """Simulate a coin toss and return the result."""
-    return {"result": random.choice(["Heads", "Tails"])}
+    return {"result": random_choice(["Heads", "Tails"])}
 
 
-@random_router.get("/random/choice/{options}")
-def select_randomly(options: str) -> dict:
+@router.get("/{options}")
+def get_random_option(options: str) -> dict:
     """Select a random option from the given list."""
     options_list = json.loads(options)
     if not options_list:
         raise HTTPException(status_code=400, detail="Invalid options")
-    return {"selected": random.choice(options_list)}
+    return {"selected": random_choice(options_list)}
 
 
-@random_router.get("/random/color")
+@router.get("/color")
 def get_color() -> dict:
     """Select a random hex color."""
-    hex_colors = [
-        "#" + "".join([random.choice('0123456789ABCDEF') for _ in range(6)])]
-    return {"color": hex_colors[0]}
+    hex_color = "#" + \
+        "".join([random_choice('0123456789ABCDEF') for _ in range(6)])
+    return {"color": hex_color}
 
 
-@random_router.get("/random/user")
-def generate_user() -> dict:
+@router.get("/user")
+def get_user() -> User:
     """Generate a random user with name, email, and password."""
-    fake = faker.Faker()
-    name = fake.name()
-    first_name, last_name = name.split()[0].lower(), name.split()[-1].lower()
-    nums = ''
-    for _ in range(random.randint(0, 4)):
-        nums += str(random.randint(0, 9))
-    email = random.choice(
-        [f"{first_name}.{last_name}@{fake.free_email_domain()}",
-         f"{first_name}.{last_name}{nums}@{fake.free_email_domain()}",
-         f"{nums}{first_name}@{fake.free_email_domain()}",
-         f"{first_name}{nums}@{fake.free_email_domain()}",
-         f"{last_name}{nums}@{fake.free_email_domain()}",
-         ])
-    user = {
-        "name": name,
-        "email": email,
-        "password": fake.password()
-    }
-    return user
+    return random_user()
 
 
-@random_router.get("/random/password/{length}")
-def generate_password(length: int) -> dict:
+@router.get("/password/{length}")
+def get_password(length: int) -> dict:
     """Generate a random password of a specified length."""
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = "".join(secrets.choice(alphabet) for _ in range(length))
-    return {"password": password}
+    return {"password": random_password(length=length)}
