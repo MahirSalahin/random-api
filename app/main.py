02d4d846d2893eb691f0944020cb75a6df307282
MahirSalahin <mahirsalahin01@gmail.com>
2024-06-14 14:03:51 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,9 @@
-from fastapi import FastAPI, Request, routing
+from fastapi import FastAPI, Response
 from routers.random import router as randoom_router
 
 app = FastAPI()
 app.include_router(randoom_router)
 
 @app.get("/")
-def get_home(request: Request) -> dict:
-    """Return the API home page with available routes."""
-    routes = {}
-    for route in request.app.routes:
-        if isinstance(route, routing.APIRoute):
-            routes[route.path] = route.name
-    meta = {
-        "api_version": "1.0",
-        "docs": "https://github.com/MahirSalahin/random_api"
-    }
-    return {"routes": routes, "meta": meta}
+def get_home() -> dict:
+    return Response("Server is running")
